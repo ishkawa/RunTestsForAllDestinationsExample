c00bd8349359f3799a179711fa058f443ed0c88e
Yosuke Ishikawa <y@ishkawa.org>
2013-10-06 21:11:42 +0900

    initial commit

---
+++ Example/Example.h
@@ -0,0 +1,13 @@
+//
+//  Example.h
+//  Example
+//
+//  Created by Yosuke Ishikawa on 10/6/13.
+//  Copyright (c) 2013 Yosuke Ishikawa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Example : NSObject
+
+@end
---
+++ Example/Example.m
@@ -0,0 +1,13 @@
+//
+//  Example.m
+//  Example
+//
+//  Created by Yosuke Ishikawa on 10/6/13.
+//  Copyright (c) 2013 Yosuke Ishikawa. All rights reserved.
+//
+
+#import "Example.h"
+
+@implementation Example
+
+@end
